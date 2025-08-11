a988132a809d681a1852c3247dc446c418d6920a
mohamedaziz <hammaarfaoui@gmail.com>
2025-08-11 17:15:43 +0100

    confighttpsforGenMemes

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -107,7 +107,7 @@ useEffect(() => {
                 <img
                   src={
                     profile?.avatar
-                      ? `http://13.60.245.143:5000/uploads/${profile.avatar}`
+                      ? `https://genmemes.duckdns.org/uploads/${profile.avatar}`
                       : "https://placehold.co/96x96?text=User"
                   }
                   alt="Avatar utilisateur"
